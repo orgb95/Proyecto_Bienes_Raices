02eeb352589d64868e2017be206f3ef74fb74990
orgb95 <76708009+orgb95@users.noreply.github.com>
2023-11-18 19:50:17 -0600

    Add files via upload

---
+++ Core/POCO/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bienes_Raices.POCO
+{
+    public class Empleado
+    {
+        public int Id_Empleado { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+    }
+}
---
+++ Core/POCO/Vivienda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bienes_Raices.POCO
+{
+    public class Vivienda
+    {
+        public int Id { get; set; }
+        public string Ubicacion { get; set; }
+        public string Tipo_Vivienda { get; set; }
+        public string Estado { get; set; }
+        public string Descripcion { get; set; }
+        public float Dimension_Ancho { get; set; }
+        public float Dimension_Largo { get; set; }
+        public double Costo { get; set; }
+    }
+}
